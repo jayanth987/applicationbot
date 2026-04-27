9ff535935659231c1d3a3e9deecdebabc88f11ce
jayanth987 <pallejayanthreddy@gmail.com>
2026-04-26 20:08:29 -0500

    removed print

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def main():
-    print("Here is a simple NumPy example:")
+
     
 
     arr = np.array([1, 2, 3, 4, 5])
-    print(f"\n1D Array: {arr}")
+
     print(f"Shape: {arr.shape}")
     
     
